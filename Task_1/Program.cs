7225acf652fd90535579c0f469178cea18c6a15e
Maria <ma.pechenkina@yandex.ru>
2022-07-06 20:45:31 +0300

    added tasts 1-2

---
+++ Task_1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+int Prompt(string message){
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+
+int M = Prompt ("Введите число M => ");
+int N = Prompt ("Введите число N => ");
+
+void PrintEvenNumbers(int numberFirst, int numberSecond)
+{
+
+    if (numberFirst >= numberSecond)
+    {
+        return;
+    }
+    if (numberFirst % 2 != 0)
+    {
+        numberFirst = numberFirst +1;
+    }
+    Console.WriteLine(numberFirst);
+    PrintEvenNumbers(numberFirst+1, numberSecond);
+}
+
+PrintEvenNumbers(M, N);
+
---
+++ Task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int Prompt(string message){
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int M = Prompt ("Введите число M => ");
+int N = Prompt ("Введите число N => ");
+
+int FindSum(int numberFirst, int numberSecond)
+{
+    if (numberFirst > numberSecond)
+    {
+        return 0;
+    }
+    return numberFirst + FindSum(numberFirst+1, numberSecond);
+}
+int result = FindSum(M, N);
+
+Console.WriteLine($"Сумма натуральных элементов в промежутке от {M} до {N} равна {result}");
+
